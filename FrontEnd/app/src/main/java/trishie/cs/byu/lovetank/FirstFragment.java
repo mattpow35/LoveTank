cb8cc845039db2d103d2c027662b319bd1de7350
Trisha Iyer <trishaiyer@Trishies-laptop.app.byu.edu>
2022-11-12 16:54:11 -0700

    connect db to front end

--- FrontEnd/app/src/main/java/trishie/cs/byu/lovetank/FirstFragment.java
+++ FrontEnd/app/src/main/java/trishie/cs/byu/lovetank/FirstFragment.java
@@ -1,19 +1,32 @@
 package trishie.cs.byu.lovetank;
 
+import redis.clients.jedis.Jedis;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
+import org.w3c.dom.Text;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 import trishie.cs.byu.lovetank.databinding.FragmentFirstBinding;
 
 public class FirstFragment extends Fragment {
 
     private FragmentFirstBinding binding;
+    private EditText wife_firstName;
+    private EditText husband_firstName;
+    private EditText wife_phoneNumber;
+    private EditText husband_phoneNumber;
+    private TextView wife_loveLanguages;
+    private TextView husband_loveLanguages;
 
     @Override
     public View onCreateView(
@@ -22,20 +35,81 @@ public class FirstFragment extends Fragment {
     ) {
 
         binding = FragmentFirstBinding.inflate(inflater, container, false);
+        wife_firstName = binding.wifeNameField;
+        husband_firstName = binding.husbandNameField;
+        wife_phoneNumber = binding.wifePhoneField;
+        husband_phoneNumber = binding.husbandsPhoneField;
+        wife_loveLanguages = binding.loveLanguageDropdown;
+        husband_loveLanguages = binding.loveLanguageDropdown2;
+
+        //        wife_firstName = (EditText) binding.findViewById(R.id.wifeNameField);
+//        husband_firstName = (EditText) view.findViewById(R.id.husbandNameField);
+//        wife_phoneNumber = (EditText) getActivity().findViewById(R.id.wifePhoneField);
+//        husband_phoneNumber = (EditText) getActivity().findViewById(R.id.husbandsPhoneField);
+//        wife_loveLanguages = (TextView) getActivity().findViewById(R.id.loveLanguageDropdown);
+//        husband_loveLanguages = (TextView) getActivity().findViewById(R.id.loveLanguageDropdown2);
+
         return binding.getRoot();
 
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
+        //button_first
         binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                Jedis jedis = new Jedis("localhost", 6379);
+                System.out.println("connected to reddis");
+
+                String wifesName = wife_firstName.getText().toString();
+                String wifes_phone = wife_phoneNumber.getText().toString();
+                String wife_languages = wife_loveLanguages.getText().toString();
+                String husbandsName = husband_firstName.getText().toString();
+                String husbands_phone = husband_phoneNumber.getText().toString();
+                String husbands_languages = husband_loveLanguages.getText().toString();
+
+                String[] arrOfHus = husbands_languages.split(", ");
+                String[] arrOfWife = wife_languages.split(", ");
+
+                System.out.println("wife name " + wifesName);
+                System.out.println("wife number " + wifes_phone);
+                System.out.println("wife love languages " + arrOfWife[0]);
+                System.out.println("wife love languages " + arrOfWife[1]);
+                System.out.println("husbands name " + husbandsName);
+                System.out.println("husbands phone " + husbands_phone );
+                System.out.println("husbands love lang " + arrOfHus[0]);
+                System.out.println("husbands love lang " + arrOfHus[1]);
+
+                //set if non existant
+                jedis.setnx(husbands_phone + ":name", husbandsName);
+                jedis.setnx(husbands_phone + ":partner_num", wifes_phone);
+                jedis.setnx(husbands_phone + ":love_lang_1", arrOfHus[0]);
+                jedis.setnx(husbands_phone + ":love_lang_2", arrOfHus[1]);
+                jedis.setnx(husbands_phone + ":partner_name", wifesName);
+                jedis.lpush("phone_numbers", husbands_phone);
+
+                jedis.setnx(wifes_phone + ":name", wifesName);
+                jedis.setnx(wifes_phone + ":partner_num", husbands_phone);
+                jedis.setnx(wifes_phone + ":love_lang_1", arrOfWife[0]);
+                jedis.setnx(wifes_phone + ":love_lang_2", arrOfWife[1]);
+                jedis.setnx(wifes_phone + ":partner_name", husbandsName);
+                jedis.lpush("phone_numbers", wifes_phone);
+
+
+
                 NavHostFragment.findNavController(FirstFragment.this)
                         .navigate(R.id.action_FirstFragment_to_SecondFragment);
             }
         });
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //assign variable
+
     }
 
     @Override
--- FrontEnd/app/src/main/java/trishie/cs/byu/lovetank/MainActivity.java
+++ FrontEnd/app/src/main/java/trishie/cs/byu/lovetank/MainActivity.java
@@ -1,27 +1,43 @@
 package trishie.cs.byu.lovetank;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
-
 import com.google.android.material.snackbar.Snackbar;
-
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.view.View;
-
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-
+import redis.clients.jedis.Jedis;
 import trishie.cs.byu.lovetank.databinding.ActivityMainBinding;
-
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
+    TextView loveLanguageDropdown;
+    TextView loveLanguageDropdown_husband;
+    boolean[] selectedLoveLanguages;
+    boolean[] selectedLoveLanguages_husband;
+    ArrayList<Integer> loveLanguagesList = new ArrayList<>();
+    ArrayList<Integer> loveLanguagesList_husband = new ArrayList<>();
+    String[] loveLangArray = {"Quality Time", "Acts of Services",
+            "Physical Touch", "Words of Affirmations", "Gifts" };
+
+    private EditText wife_firstName;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +59,133 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        //assign variable
+        loveLanguageDropdown = findViewById(R.id.loveLanguageDropdown);
+        //assign husbands variables
+        loveLanguageDropdown_husband = findViewById(R.id.loveLanguageDropdown2);
+
+        wife_firstName = findViewById(R.id.wifeNameField);
+        System.out.println("I AM GEREEEE");
+        System.out.println(wife_firstName.getText().toString());
+
+        //------------------------------------
+
+        //initialize love language array
+        selectedLoveLanguages = new boolean[loveLangArray.length];
+        selectedLoveLanguages_husband = new boolean[loveLangArray.length];
+
+        loveLanguageDropdown.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //initialize alert dialog
+                AlertDialog.Builder builder = new AlertDialog.Builder(
+                        MainActivity.this
+                );
+                //set title
+                builder.setTitle("Select Love Language");
+                //set dialog non-cancellable
+                builder.setCancelable(false);
+                builder.setMultiChoiceItems(loveLangArray, selectedLoveLanguages, new DialogInterface.OnMultiChoiceClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+                        //check condition -- whether box is checked
+                        if (isChecked){
+                            loveLanguagesList.add(which);
+                            //sort lang list
+                            Collections.sort(loveLanguagesList);
+                        }
+                        else{
+                            //when box is unselected
+                            loveLanguagesList.remove(which);
+                        }
+                    }
+                });
+
+                builder.setPositiveButton("done", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //initialize string builder
+                        StringBuilder stringBuilder = new StringBuilder();
+
+                        //for every one the user has chosen
+                        for(int j = 0; j < loveLanguagesList.size(); j++){
+                            //use that particular index of the list to get actual love lang name
+                            stringBuilder.append(loveLangArray[loveLanguagesList.get(j)]);
+                            //check conditon
+                            if (j != loveLanguagesList.size()-1){
+                                stringBuilder.append(", ");
+                            }
+                        }
+                        loveLanguageDropdown.setText(stringBuilder.toString());
+                    }
+                });
+
+                builder.show();
+            }
+        });
+        //-------------------------------------------------------------------------------------------
+        loveLanguageDropdown_husband.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //initialize alert dialog
+                AlertDialog.Builder builder = new AlertDialog.Builder(
+                        MainActivity.this
+                );
+                //set title
+                builder.setTitle("Select Love Language");
+                //set dialog non-cancellable
+                builder.setCancelable(false);
+                builder.setMultiChoiceItems(loveLangArray, selectedLoveLanguages_husband, new DialogInterface.OnMultiChoiceClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+                        //check condition -- whether box is checked
+                        if (isChecked){
+                            loveLanguagesList_husband.add(which);
+                            //sort lang list
+                            Collections.sort(loveLanguagesList_husband);
+                        }
+                        else{
+                            //when box is unselected
+                            loveLanguagesList_husband.remove(which);
+                        }
+                    }
+                });
+
+                builder.setPositiveButton("done", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //initialize string builder
+                        StringBuilder stringBuilder = new StringBuilder();
+
+                        //for every one the user has chosen
+                        for(int j = 0; j < loveLanguagesList_husband.size(); j++){
+                            //use that particular index of the list to get actual love lang name
+                            stringBuilder.append(loveLangArray[loveLanguagesList_husband.get(j)]);
+                            //check conditon
+                            if (j != loveLanguagesList_husband.size()-1){
+                                stringBuilder.append(", ");
+                            }
+                        }
+                        loveLanguageDropdown_husband.setText(stringBuilder.toString());
+                    }
+                });
+
+                builder.show();
+            }
+        });
+
+//        binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//
+////                Jedis jedis = new Jedis("localhost", 6379);
+////                System.out.println("connected to reddis");
+//
+//                NavHostFragment.findNavController(FirstFragment.this)
+//                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
+//            }
+//        });
     }
 
     @Override
@@ -73,4 +216,6 @@ public class MainActivity extends AppCompatActivity {
         return NavigationUI.navigateUp(navController, appBarConfiguration)
                 || super.onSupportNavigateUp();
     }
+
+
 }
